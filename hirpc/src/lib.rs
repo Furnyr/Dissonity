bf1da9e862726cbbb368d9106aad39190780e4c4
nyrrren <118135776+Furnyr@users.noreply.github.com>
2024-11-10 01:03:44 +0100

    ci: fix tests (#17)
    * ci: fix tests
    
    * ci: update chrome version

--- hirpc/src/lib.rs
+++ hirpc/src/lib.rs
@@ -9,7 +9,7 @@ Its purpose is:
 
 std is only used for essential functionality.
 
-- - - - -
+- - - - - -
 
 modules/rpc.rs -> Connection with the RPC protocol
 modules/public.rs -> Exposed API and hashes
