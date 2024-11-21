1b64d47ae587e7478ff121335d99110d1b8ee833
Furnyr <developnyrrr@gmail.com>
2024-11-21 18:01:01 +0100

    fix(website): update base url

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://Furnyr.github.io/Dissonity"
+  base: "https://dissonity.dev"
 })
