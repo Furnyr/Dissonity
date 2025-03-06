a57e7a579749d2cab46a65d74fa1f1065127dfc5
Rinne <99666293+Semanual@users.noreply.github.com>
2025-03-06 12:27:25 -0300

    Added GetApplicationId function (#31)
    * Added GetApplicationId function
    
    * fix: send id in discord bridge
    
    * style: add newline after method
    
    * chore: delete package-lock.json
    
    pnpm is used for development, so the lock file is pnpm-lock.yaml
    
    * chore: add compatibility check
    
    ---------
    
    Co-authored-by: Furnyr <developnyrrr@gmail.com>

--- unity/Runtime/Config.cs
+++ unity/Runtime/Config.cs
@@ -3,6 +3,6 @@ namespace Dissonity
 {
     public static class Config
     {
-        public readonly static string version = "1.1.4";
+        public readonly static string version = "1.2.0";
     }
 }
